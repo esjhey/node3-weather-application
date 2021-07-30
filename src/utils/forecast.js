70fb13d366cd1a8b1642a549114cab79ff3704e4
esjhey <ms.sarahespanola@gmail.com>
2021-07-30 15:50:25 +0800

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,25 @@
+const request = require('request');
+
+const forecast = (latitude, longitude, callback) => {
+  const url =
+    'http://api.weatherstack.com/current?access_key=759f79f348106f873f2a6ffc68195f8c&query=' +
+    latitude +
+    ',' +
+    longitude +
+    '&units=m';
+
+  request({ url, json: true }, (error, { body }) => {
+    if (error) {
+      callback('Unable to connect to weather service', undefined);
+    } else if (body.error) {
+      callback('Unable to find the locion', undefined);
+    } else {
+      callback(
+        undefined,
+        `${body.current.weather_descriptions}. It is currently ${body.current.temperature} degrees out. There is a ${body.current.feelslike}% chance of rain `
+      );
+    }
+  });
+};
+
+module.exports = forecast;
